e41e60a34da19e2a1522d58e686c06d75cdcbeea
Julio Noguera <noguerajulio24@gmail.com>
2020-02-01 23:04:46 -0500

    use UseEffect and localStorage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
 import Cita from './components/Cita';
 
 function App() {
 
+  // Citas en local storage
+  let citasInicials = JSON.parse(localStorage.getItem('citas'));
+  if(!citasInicials){
+    citasInicials = [];
+  }
+
   // Arreglo de citas
-  const [citas, guardarCitas ] = useState([]);
+  const [citas, guardarCitas ] = useState(citasInicials);
   
+  // Use effect para realizar ciertas operaciones cuando el state cambia
+
+  useEffect( () => {
+    if(citasInicials){
+      localStorage.setItem('citas', JSON.stringify(citas));
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  }, [citas] );
+
   // función que tome las citas actuales y agregue la nueva
   const crearCita = cita => {
     guardarCitas([
